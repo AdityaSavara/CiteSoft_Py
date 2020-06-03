f169567d73fa5ebc44d954f7daaa0f420e9b2128
Christopher Hayes <cphayes@optonline.net>
2020-06-03 16:55:50 -0400

    Initial implementation complete, simple test cases added

--- CiteSoftWorkingDirectory.py
+++ CiteSoftWorkingDirectory.py
@@ -9,15 +9,23 @@
 #------------------------------------------------------------------------------------------------------------
 from datetime import datetime
 import yaml
+import semantic_version
+import re
+
+#Dependencies for testing only
+from random import randint
 
 _citations = {}
 _OUTPUT_FILE_NAME = "CiteSoftwareCheckPoints.txt"
-_CONSOLIDATED_FILE_NAME = ""
+_CONSOLIDATED_FILE_NAME = "CiteSoftwareConsolidatedLog.txt"
 
-def module_call_cite(func, unique_id, software_name, **kwargs):
-    def inner(unique_id, software_name, **kwargs):
-        import_cite(unique_id, software_name, kwargs)
-        func()
+def module_call_cite(unique_id, software_name, **add_args):
+    def inner(func):
+        def call(*args, **kwargs):
+            import_cite(unique_id, software_name, **add_args)
+            result = func(*args, **kwargs)
+            return result
+        return call
     return inner
 
 def import_cite(unique_id, software_name, **kwargs):
@@ -35,23 +43,40 @@ def add_citation(unique_id, software_name, **kwargs):
 
 def compile_cite_software_log():
     with open(_OUTPUT_FILE_NAME, 'a') as file:
-        file.write('---\r\n')
-        print("loop")
-        for key,dict in _citations.items():
-            file.write('-\r\n')
-            for s in {'timestamp', 'unique_id', 'software_name', 'version'}:
-                file.write('    ' + s + ': >-\r\n')
-                file.write('        ' + dict[s] + '\r\n')
-            for subkey in dict:
-                if subkey not in {'timestamp', 'unique_id', 'software_name', 'version'}:
-                    file.write('    ' + subkey + '\r\n')
-                    file.write('        ' + dict[subkey] + '\r\n')
-#TODO : Syntax of exporting optional fields
+        write_dict_to_output(file, _citations)
 
 def consolidate_software_log():
+    consolidated_dict = {}
     with open(_OUTPUT_FILE_NAME) as file:
-        file_contents = yaml.safe_load(file)
-    #TODO : Consolidation code
+        file_contents = yaml.safe_load_all(file)
+        for yaml_file in file_contents:
+            for item in yaml_file:
+                id = item["unique_id"]
+                if id in consolidated_dict:
+                    consolidated_dict[id] = compare_same_id(consolidated_dict[id], item)
+                else:
+                    consolidated_dict[id] = item
+    with open(_CONSOLIDATED_FILE_NAME, 'w') as file:
+        write_dict_to_output(file, consolidated_dict)
+
+#Takes a dictionary, converts it to CiteSoft-compatible YAML, and writes it to file
+def write_dict_to_output(file, dictionary):
+    file.write('---\r\n')
+    for key,dict in dictionary.items():
+        file.write('-\r\n')
+        for s in {'timestamp', 'unique_id', 'software_name', 'version'}:
+            file.write('    ' + s + ': >-\r\n')
+            file.write('    '*2 + dict[s] + '\r\n')
+        for subkey in dict:
+            if subkey not in {'timestamp', 'unique_id', 'software_name', 'version'}:
+                file.write('    ' + subkey + ':\r\n')
+                if type(dict[subkey]) is list:
+                    for i in dict[subkey]:
+                        file.write('    '*2 + '- >-\r\n')
+                        file.write('    '*3 + i + '\r\n')
+                else:
+                    file.write('    '*2 + '- >-\r\n')
+                    file.write('    '*3 + dict[subkey] + '\r\n')
 
 #Helper Functions
 
@@ -61,28 +86,99 @@ def get_timestamp():
     timestamp = now.strftime("%Y-%m-%dT%H:%M:%S")
     return timestamp
 
+#Compares two entries
+#Returns : The entry which should be kept
 def compare_same_id(old_entry, new_entry):
-    old_has_verison = "version" in old_entry
+    old_has_version = "version" in old_entry
     new_has_version = "version" in new_entry
     if old_has_version and new_has_version:#If both entries have a version, compare them return and the return the greater(newer) version
-        if old_entry["version"] > new_entry["version"]:
-            return old_entry
-        else:
-            return new_entry
+        old_ver_str = old_entry["version"]
+        new_ver_str = new_entry["version"]
+        try:
+            old_sv = semantic_version.Version(old_ver_str)
+            new_sv = semantic_version.Version(new_ver_str)
+            if old_sv > new_sv:
+                return old_entry
+            else:
+                return new_entry
+        except ValueError:
+            regex = "[0-9]*\.[0-9]*"
+            if re.match(regex, old_ver_str) and re.match(regex, new_ver_str):
+                try:
+                    old_ver_float = float(old_ver_str)
+                    new_ver_float = float(new_ver_str)
+                    if old_ver_float > new_ver_float:
+                        return old_entry
+                    else:
+                        return new_entry
+                except ValueError:
+                    if old_ver_str > new_ver_str:
+                        return old_entry
+                    else:
+                        return new_entry
+            else:
+                if old_ver_str > new_ver_str:
+                    return old_entry
+                else:
+                    return new_entry
     elif old_has_version and not new_has_version:#If old entry has a version and the new entry doesn't, the entry with a version takes precedence
         return old_entry
     elif not old_has_version and new_has_version:#Likewise, if new entry has a version and the old entry doesn't, the entry with a version takes precedence
         return new_entry
-    else:#If neither entry has a version, don't replace the existing entry
-        return old_entry
+    else:#If neither entry has a version, save the new entry
+        return new_entry
 
 def main():
-    print("Hello World!")
+    print("Welcome to CiteSoft!")
+    print("Running test cases...")
+    test_inline_citation()
+    test_wrapper_func()
+    test_semantic_version()
+    test_decimal_version()
+    print("Appending citations to data file")
+    compile_cite_software_log()
+    consolidate_software_log()
+
+#Test Cases
+def test_inline_citation():
+    print("Testing inline citations")
     unique_id = "TestID"
     software_name = "CiteSoft"
     import_cite(unique_id, software_name)
-    print(_citations)
-    compile_cite_software_log()
+
+def test_semantic_version():
+    print("Testing semantic version comparison")
+    unique_id = "semantic_ver_test"
+    software_name = "CiteSoft"
+    for _ in range(1, 4):
+        kwargs = {"version": str(randint(0,10)) + '.' + str(randint(0,10)) + '.' + str(randint(0,10))}
+        print("Adding citation with version : " + kwargs["version"])
+        import_cite(unique_id, software_name, **kwargs)
+
+def test_decimal_version():
+    print("Testing decimal version comparison")
+    unique_id = "decimal_ver_test"
+    software_name = "CiteSoft"
+    for _ in range(1, 4):
+        kwargs = {"version": str(randint(0,10)) + '.' + str(randint(0,10))}
+        print("Adding citation with version : " + kwargs["version"])
+        import_cite(unique_id, software_name, **kwargs)
+
+def test_wrapper_func():
+    print("Testing wrapper function")
+    a = 5
+    b = 12
+    print("Adding %d and %d", a, b)
+    res = func_cite_test_add(a, b)
+    print("Result : %d", res)
+
+unique_id = "func_cite_test_add"
+software_name = "CiteSoft"
+kwargs = {"version": "1.1.5", "author": "CPH", "language": "Python"}
+@module_call_cite(unique_id, software_name, **kwargs)
+def func_cite_test_add(x, y):
+    return x + y
+
 
 if __name__ == '__main__':
     main()
