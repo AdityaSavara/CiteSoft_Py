a04d78553f4440879206545603c32f0645580685
Christopher Hayes <cphayes@optonline.net>
2020-06-09 11:53:36 -0400

    Improvements to version comparison, fixed add citation bug

--- CiteSoftWorkingDirectory.py
+++ CiteSoftWorkingDirectory.py
@@ -46,6 +46,8 @@ def add_citation(unique_id, software_name, **kwargs):
         if _VALIDATE_OPT_ARG:
             if not key in _VALID_OPT_ARG:
                 eprint("Warning, " + key + " is not an officially supported argument name.  Use of alternative argument names is strongly discouraged.")
+        if type(kwargs[key]) is not list:#Make sure single optional args are wrapped in a list
+            kwargs[key] = [kwargs[key]]
         new_entry[key] = kwargs[key]
     if unique_id in _citations:#Check for duplicate entries(e.g. from calling the same function twice)
         _citations[unique_id] = compare_same_id(_citations[unique_id], new_entry)
@@ -112,31 +114,40 @@ def compare_same_id(old_entry, new_entry):
     old_has_version = "version" in old_entry
     new_has_version = "version" in new_entry
     if old_has_version and new_has_version:#If both entries have a version, compare them return and the return the greater(newer) version
-        old_ver_str = str(old_entry["version"])
-        new_ver_str = str(new_entry["version"])
+        old_ver_str = str(old_entry["version"][0])
+        new_ver_str = str(new_entry["version"][0])
+        old_ver_semver_valid = True
+        new_ver_semver_valid = True
         try:
             old_sv = semantic_version.Version(old_ver_str)
+        except ValueError:
+            old_ver_semver_valid = False
+        try:
             new_sv = semantic_version.Version(new_ver_str)
+        except:
+            new_ver_semver_valid = False
+        if old_ver_semver_valid and new_ver_semver_valid:#If both entries have a valid SemVer version, keep the newer one
             if old_sv >= new_sv:
                 return old_entry
             else:
                 return new_entry
-        except ValueError:
-            regex = "[0-9]*\.[0-9]*"
-            if re.match(regex, old_ver_str) and re.match(regex, new_ver_str):
-                try:
+        elif old_ver_semver_valid:#If only the old entry has a valid SemVer version, keep it
+            return old_entry
+        elif new_ver_semver_valid:#If only the new entry has a valid SemVer version, keep it
+            return new_entry
+        else:#If neither entry has a valid semver version, try decimal comparison
+            try:
+                regex = "[0-9]*\.[0-9]*"
+                if re.match(regex, old_ver_str) and re.match(regex, new_ver_str):
                     old_ver_float = float(old_ver_str)
                     new_ver_float = float(new_ver_str)
                     if old_ver_float >= new_ver_float:
                         return old_entry
                     else:
                         return new_entry
-                except ValueError:
-                    if old_ver_str >= new_ver_str:
-                        return old_entry
-                    else:
-                        return new_entry
-            else:
+                else:
+                    raise ValueError('Regex check failed')
+            except ValueError:#Decimal comparison failed, use alphanumeric comparison
                 if old_ver_str >= new_ver_str:
                     return old_entry
                 else:
